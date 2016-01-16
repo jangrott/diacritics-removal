264d4ada4831d6063723a068b908a8ec29c4bff9
jangrott <tomek.jangrot@gmail.com>
2016-01-16 01:33:11 +0100

    extract exceptions to list to make code better

--- src/main/groovy/pl/jangrot/diacriticsremoval/DiacriticsDeleter.groovy
+++ src/main/groovy/pl/jangrot/diacriticsremoval/DiacriticsDeleter.groovy
@@ -4,11 +4,25 @@ import org.apache.commons.lang3.StringUtils;
 
 class DiacriticsDeleter {
 
+    static exceptions = [
+            { string -> string.replaceAll("ł", "l")},
+            { string -> string.replaceAll("Ł", "L")}
+    ]
     static removeDiacritics = { StringUtils.stripAccents(it) }
 
     static deleteFrom(String string) {
-        removeDiacritics(string)
-                .replaceAll("ł", "l")
-                .replaceAll("Ł", "L")
+        removeDiacriticsAndApplyExceptions(string)
+    }
+
+    static removeDiacriticsAndApplyExceptions(string) {
+        applyExceptions(removeDiacritics(string))
+    }
+
+    static applyExceptions(string) {
+        def output = string
+        exceptions.each {
+            output = it(output)
+        }
+        output
     }
 }
